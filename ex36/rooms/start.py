523a415f12704c9cc2758e6efea80c728592fad9
Piotr Bogdan <bogdzio@o2.pl>
2015-10-15 22:25:27 +0200

    Start room finished.

--- ex36/rooms/start.py
+++ ex36/rooms/start.py
@@ -11,8 +11,16 @@ def start():
         proper_choice = crossroads()
         
 def crossroads():
+    """
+    Repeatable choice checker. If cannot determine, what
+    should be done, returns False.
+    """
     decision = def_input()
     if decision == '1' or decision == '1.':
         creek()
-    
+    elif decision == '2' or decision == '2.':
+        dragon()
+    else:
+        print "Unknown choice. Try again."
+        return False
 
